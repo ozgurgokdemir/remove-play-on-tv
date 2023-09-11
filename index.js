1104db22937e83076426886fd20e1c14630d716a
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-09-12 01:40:08 +0300

    feat: Check the Button with MutationObserver

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const remoteButton = document.querySelector('.ytp-remote-button');
-if (remoteButton) remoteButton.remove();
+const observer = new MutationObserver((mutations, observer) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+      const remoteButton = document.querySelector('.ytp-remote-button');
+      if (!remoteButton) return;
+      remoteButton.remove();
+      observer.disconnect();
+    }
+  });
+});
+
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true,
+});
